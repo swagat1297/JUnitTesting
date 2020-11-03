6ed722b7d7daa6a320805a25b5c5701911d62f43
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-03 18:37:25 +0530

    Added: First Name file

--- FirstName.java
+++ FirstName.java
@@ -8,10 +8,8 @@ public class FirstName {
 	public boolean CheckFirstName(String firstName) {
 		String read = "[A-Z][a-z]{3,}";
 		String text = firstName;
-		
 		Pattern pattern = Pattern.compile(read);
 		Matcher match = pattern.matcher(text);
-		
 		boolean result = match.matches();
 		return (result);
 	}
