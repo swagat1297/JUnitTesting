f3625d05a0f9fc81aad035fc2c1f60ea6d756175
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-05 09:52:56 +0530

    Updated: password file

--- Password.java
+++ Password.java
@@ -6,14 +6,20 @@ import java.util.regex.Pattern;
 public class Password {
 	//check password with Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character//
 		public boolean CheckPassword(String Password) {
-			String read = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
-			String text = Password;
+			try{
+				String read = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+				String text = Password;
 			
-			Pattern pattern = Pattern.compile(read);
-			Matcher match = pattern.matcher(text);
+				Pattern pattern = Pattern.compile(read);
+				Matcher match = pattern.matcher(text);
 			
-			boolean result = match.matches();
-			return (result);						
+				boolean result = match.matches();
+				return (result);
+			}
+			catch(Exception e){
+				return false;
+			}
+			
 		}
 
 }
