10d5e84432c9dd2cb2db55dfc40b50dae8af57eb
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-05 09:48:27 +0530

    Updated: Last Name code

--- LastName.java
+++ LastName.java
@@ -6,15 +6,19 @@ import java.util.regex.Pattern;
 public class LastName {
 	         //to check valid last name//
 		public boolean CheckLastName(String lastName) {
-			String read = "[A-Z][a-z]{3,}";
-			String text = lastName;
+			try{
+				String read = "[A-Z][a-z]{3,}";
+				String text = lastName;
 			
-			Pattern pattern = Pattern.compile(read);
-			Matcher match = pattern.matcher(text);
+				Pattern pattern = Pattern.compile(read);
+				Matcher match = pattern.matcher(text);
 			
-			boolean result = match.matches();
-			return (result);
-			
+				boolean result = match.matches();
+				return (result);
+			}
+			catch(Exception e){
+				return false;
+			}
 		}
 
 }
